a5b5cf43442ae949fde0dea485141a5062cc455d
G_GomzaNR <gomza.natella@gmail.com>
2025-08-19 18:29:23 +0300

    add table, modal, mocks

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,88 +1,147 @@
-import React from 'react';
-import { Space, Table, Tag } from 'antd';
-import type { TableProps } from 'antd';
+import React, {useEffect, useState} from 'react';
+import {Button, Space, Table, Modal, Form, Input, InputNumber, DatePicker} from 'antd';
+import type {TableProps} from 'antd';
+import {DeleteOutlined, EditOutlined, PlusOutlined} from '@ant-design/icons';
 
 interface DataType {
     key: string;
     name: string;
-    age: number;
-    address: string;
-    tags: string[];
+    date: string;
+    amount: number;
 }
 
-const columns: TableProps<DataType>['columns'] = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        key: 'name',
-        render: (text) => <a>{text}</a>,
-    },
-    {
-        title: 'Age',
-        dataIndex: 'age',
-        key: 'age',
-    },
-    {
-        title: 'Address',
-        dataIndex: 'address',
-        key: 'address',
-    },
-    {
-        title: 'Tags',
-        key: 'tags',
-        dataIndex: 'tags',
-        render: (_, { tags }) => (
-            <>
-                {tags.map((tag) => {
-                    let color = tag.length > 5 ? 'geekblue' : 'green';
-                    if (tag === 'loser') {
-                        color = 'volcano';
-                    }
-                    return (
-                        <Tag color={color} key={tag}>
-                            {tag.toUpperCase()}
-                        </Tag>
-                    );
-                })}
-            </>
-        ),
-    },
-    {
-        title: 'Action',
-        key: 'action',
-        render: (_, record) => (
-            <Space size="middle">
-                <a>Invite {record.name}</a>
-                <a>Delete</a>
-            </Space>
-        ),
-    },
-];
+type CustomColumn = NonNullable<TableProps<DataType>['columns']>[number] & {
+    inputType?: 'text' | 'number' | 'date';
+    title: string,
+    dataIndex: string,
+    required: boolean,
+};
 
-const data: DataType[] = [
-    {
-        key: '1',
-        name: 'John Brown',
-        age: 32,
-        address: 'New York No. 1 Lake Park',
-        tags: ['nice', 'developer'],
-    },
-    {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park',
-        tags: ['loser'],
-    },
-    {
-        key: '3',
-        name: 'Joe Black',
-        age: 32,
-        address: 'Sydney No. 1 Lake Park',
-        tags: ['cool', 'teacher'],
-    },
-];
+const App: React.FC = () => {
+    const [dataSource, setDataSource] = useState<DataType[]>([]);
+    const [openModal, setOpenModal] = useState<boolean>(false);
+    const [disabled, setDisabled] = useState<boolean>(true);
+    const [loadingData, setLoadingData] = useState<boolean>(false);
+    const [form] = Form.useForm();
 
-const App: React.FC = () => <Table<DataType> columns={columns} dataSource={data} />;
+    const columns: CustomColumn[] = [
+        {
+            title: "Имя",
+            dataIndex: 'name',
+            key: 'name',
+            inputType: 'text',
+            required: true,
+        },
+        {
+            title: 'Дата',
+            dataIndex: 'date',
+            key: 'date',
+            inputType: 'date',
+            required: true,
+        },
+        {
+            title: 'Количество',
+            dataIndex: 'amount',
+            key: 'amount',
+            inputType: 'number',
+            required: true,
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            dataIndex: 'action',
+            required: false,
+            render: (_, record) => (
+                <Space size="middle">
+                    <EditOutlined/>
+                    <DeleteOutlined style={{color: 'red'}}/>
+                </Space>
+            ),
+        },
+    ];
+
+    const modalFields = columns.filter(col => col.inputType);
+
+    useEffect(() => {
+        getList();
+    }, []);
+
+    const getList = () => {
+        setLoadingData(true)
+        fetch('/api/record/list')
+            .then(res => res.json())
+            .then(data => {
+                setDataSource(data.data);
+                setLoadingData(false);
+            });
+    }
+
+    const handleOk = async () => {
+        try {
+            await form.validateFields();
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+
+        const newRecord = form.getFieldsValue();
+
+        setLoadingData(true);
+        fetch('/api/record', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(newRecord),
+        })
+            .then(response => response.json())
+            .then(result => {
+                if (result.code === 0) {
+                    setLoadingData(false);
+                    form.resetFields();
+                    setOpenModal(false);
+                    getList();
+                } else {
+                    console.error('Ошибка при добавлении');
+                }
+            })
+            .catch(error => console.error('Ошибка запроса:', error));
+    }
+
+    return (<>
+        <Button type={'primary'} icon={<PlusOutlined/>} onClick={() => setOpenModal(true)}>Добавить</Button>
+        <Modal
+            open={openModal}
+            onCancel={() => {
+                setOpenModal(false);
+                form.resetFields();
+            }}
+            title={"Добавить запись"}
+            okText={"Добавить"}
+            cancelText={"Отмена"}
+            onOk={() => handleOk()}
+            okButtonProps={{
+                loading: loadingData,
+            }}
+        >
+            <Form layout="vertical" form={form}>
+                {modalFields.map(col => (
+                    <Form.Item
+                        label={col.title}
+                        name={col.dataIndex}
+                        key={col.key}
+                        rules={[{required: col.required, message: "Поле обязательно для заполнения"}]}
+                    >
+                        {col.inputType === 'number' ? <InputNumber style={{width: '100%'}}/> :
+                            col.inputType === 'date' ? <DatePicker style={{width: '100%'}}/> :
+                                <Input/>
+                        }
+                    </Form.Item>
+                ))}
+            </Form>
+        </Modal>
+        <Table<DataType> columns={columns} dataSource={dataSource} loading={loadingData}/>
+    </>)
+};
 
 export default App;
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React, {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import dayjs from 'dayjs';
+import 'dayjs/locale/ru';
+import {ConfigProvider} from "antd";
+import ruRU from 'antd/es/locale/ru_RU';
+dayjs.locale('ru');
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <ConfigProvider locale={ruRU}>
+        <StrictMode>
+            <App/>
+        </StrictMode>
+    </ConfigProvider>,
 )
